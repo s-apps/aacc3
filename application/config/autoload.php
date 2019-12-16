07ab3433f4f343ac09a11fc801b0441157b31215
Silverio O Paula <contato@silverio.eti.br>
2019-12-16 14:28:14 -0300

    base_url

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('Twig','session','database','constantes');
+$autoload['libraries'] = array('Twig', 'database', 'session', 'constantes');
 
 /*
 | -------------------------------------------------------------------
