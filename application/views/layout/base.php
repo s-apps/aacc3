3ed785c5b84ec39c9aff759f07e0db99b5e2d6f7
Silverio O Paula <contato@silverio.eti.br>
2019-12-19 15:24:11 -0300

    template adjust - dashboard

--- application/views/layout/base.php
+++ application/views/layout/base.php
@@ -9,13 +9,13 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ constant('BASE_URL') }}assets/img/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ constant('BASE_URL') }}assets/img/favicon-16x16.png">
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/bootstrap-table/bootstrap-table.min.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/css/all.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/select2/css/select2.min.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/select2/css/select2-bootstrap.min.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/tempusdominus/css/tempusdominus-bootstrap-4.min.css">
-    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/izitoast/css/iziToast.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/js/bootstrap-table/bootstrap-table.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/css/fontawesome/all.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/js/select2/css/select2.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/js/select2/css/select2-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/js/tempusdominus/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/js/izitoast/css/iziToast.min.css">
     <link rel="stylesheet" href="{{ constant('BASE_URL') }}assets/css/custom.css">
   </head>
   <body class="d-flex flex-column h-100">
@@ -49,17 +49,17 @@
     
     {% block scripts %}
       <script src="{{ constant('BASE_URL') }}assets/js/jquery-3.4.1.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/js/popper.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap.min.js"></script>   
-      <script src="{{ constant('BASE_URL') }}assets/select2/js/select2.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/bootstrap-table/bootstrap-table.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/bootstrap-table/locale/bootstrap-table-pt-BR.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/table-export/tableExport.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/bootstrap-table/extensions/export/bootstrap-table-export.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/moment/moment.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/moment/locale/pt-br.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-      <script src="{{ constant('BASE_URL') }}assets/izitoast/js/iziToast.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap/popper.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap/bootstrap.min.js"></script>   
+      <script src="{{ constant('BASE_URL') }}assets/js/select2/js/select2.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap-table/bootstrap-table.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap-table/locale/bootstrap-table-pt-BR.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/table-export/tableExport.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/bootstrap-table/extensions/export/bootstrap-table-export.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/moment/moment.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/moment/locale/pt-br.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+      <script src="{{ constant('BASE_URL') }}assets/js/izitoast/js/iziToast.min.js"></script>
       <script src="{{ constant('BASE_URL') }}assets/js/aacc/comum.js"></script>	
     {% endblock %}
 
