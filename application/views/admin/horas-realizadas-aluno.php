b21e583c4fce48f17310a9918fd632d9f480dbdb
Silverio O Paula <contato@silverio.eti.br>
2019-12-30 16:31:10 -0300

    horas realizadas

--- application/views/admin/horas-realizadas-aluno.php
+++ application/views/admin/horas-realizadas-aluno.php
@@ -7,23 +7,26 @@
     <h5 class="card-title"><i class="fas fa-clock"></i> Aluno - Horas realizadas</h5>
     <hr>
     <div class="row">
-        <div class="col-6">
-            <label for="usuario_ra">Aluno</label>
-                <select id="usuario_id" name="usuario_id" class="form-control">
-                    <option></option>
-                    {% for usuario in usuarios %}
-                    <option value="{{ usuario.usuario_id }}">{{ usuario.usuario_ra }} | {{ usuario.nome }} | {{ usuario.email }}</option>
-                    {% endfor %}
-                </select>
-        </div>
+      <div class="col-6">
+        <label for="usuario_ra">Aluno</label>
+        <select id="usuario_id" name="usuario_id" class="form-control">
+          <option></option>
+          {% for usuario in usuarios %}
+          <option value="{{ usuario.usuario_id }}">{{ usuario.usuario_ra }} | {{ usuario.nome }} | {{ usuario.email }}</option>
+          {% endfor %}
+        </select>
+      </div>
+      <div class="col-6">
+        <div style="margin-top: 2rem;" id="totalHorasRealizadas"></div>
+      </div>
     </div>
-    <table id="horas-realizadas"></table>   
+    <table id="horas-realizadas-aluno" class="mt-3"></table>
   </div>
 </div>
 {% endblock %}
 
 {% block scripts %}
-  {{ parent() }}
-  <script src="{{ constant('BASE_URL') }}assets/js/aacc/aluno-horas-realizadas.js"></script>
-  <script src="{{ constant('BASE_URL') }}assets/js/aacc/comum.js"></script>
+{{ parent() }}
+<script src="{{ constant('BASE_URL') }}assets/js/aacc/aluno-horas-realizadas.js"></script>
+<script src="{{ constant('BASE_URL') }}assets/js/aacc/comum.js"></script>
 {% endblock %}
--- assets/js/aacc/aluno-horas-realizadas.js
+++ assets/js/aacc/aluno-horas-realizadas.js
@@ -1,6 +1,21 @@
-var $table = $("#horas-realizadas");
+var $table = $("#horas-realizadas-aluno");
 
 $(document).ready(function(){
+  $table.bootstrapTable({
+    showColumns: false,
+    pagination: true,
+    pageList: [10, 25, 50, 100],
+    pageSize: 10,
+    theadClasses: "thead-light",
+    classes: "table table-bordered table-sm table-hover",
+    columns: [
+      {field: "data", title: "Data"},
+      {field: "atividade", title: "Atividade"},
+      {field: "horas_inicio", title: "Início"},
+      {field: "horas_termino", title: "Término"},
+      {field: "carga_horaria", title: "Carga horária"}
+    ]
+  });
   $("#usuario_id").select2({
     placeholder: "Selecione o aluno",
     theme: "bootstrap",
@@ -10,7 +25,6 @@ $(document).ready(function(){
       }
     }
   });
-  $table.bootstrapTable();
 });
 
 $("#usuario_id").on("select2:select", function (e) {
@@ -23,16 +37,42 @@ $("#usuario_id").on("select2:select", function (e) {
     data: { usuario_id: data.id }
   })
   .done(function(data){
+    $table.bootstrapTable("removeAll");
+    var horas = [];
     console.log(data.horasRealizadas);
-    $.each(data, function (key, atividade) {
+    $.each(data.horasRealizadas, function (key, atividade) {
+      horas.push({carga_horaria: atividade.carga_horaria});
       $table.bootstrapTable("insertRow", {
         index: 0,
         row: {
           data: atividade.data,
           atividade: atividade.atividade,
+          horas_inicio: atividade.horas_inicio,
+          horas_termino: atividade.horas_termino,
           carga_horaria: atividade.carga_horaria
         }
-     });
+      });
+    });
+    $("#totalHorasRealizadas").html("Total de horas: <strong>" + somaHoras(horas) + "</strong>");
   });
 });
-});
+
+function somaHoras(horas) {
+    var t1 = "00:00";
+    var mins = 0;
+    var hrs = 0;
+    $.each(horas, function (index, row) {
+        t1 = t1.split(":");
+        var t2 = row.carga_horaria.split(":");
+        mins = Number(t1[1]) + Number(t2[1]);
+        minhrs = Math.floor(parseInt(mins / 60));
+        hrs = Number(t1[0]) + Number(t2[0]) + minhrs;
+        mins = mins % 60;
+        t1 = hrs.padDigit() + ':' + mins.padDigit();
+    });
+    return t1.toString();
+}
+
+Number.prototype.padDigit = function () {
+    return (this < 10) ? '0' + this : this;
+}
